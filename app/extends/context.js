8739ef77ffeda88a48f7f1a8d80d94a4bf842378
kk <kk@youzan.com>
2018-05-01 11:34:54 +0800

    fix: getService

--- app/extends/context.js
+++ app/extends/context.js
@@ -5,39 +5,12 @@ const assert = require('assert');
 
 module.exports = {
 
-  /**
-   * 获取配置信息，参数 key 可以是点分隔符隔开的字符串，例如 foo.bar
-   * @param {String} key 配置 key
-   */
-  getConfig(key) {
-    if (!key) {
-      return this.app.config;
-    } else {
-      let keys = key.split('.');
-      let result = this.app.config;
-      let item;
-      while (item = keys.shift()) {
-        result = result[item];
-      }
-      return result;
-    }
+  getConfig(...args) {
+    return this.app.getConfig(...args);
   },
 
-  /**
-   * 获取 Service 类
-   * @param {String} packageName 包名
-   * @param {String} serviceName 服务名
-   */
-  getServiceClass(packageName, serviceName) {
-    assert(packageName, 'Package name cannot be empty!');
-    assert(serviceName, 'Service name cannot be empty!');
-    if (this.app.services
-      && this.app.services[packageName]
-      && this.app.services[packageName][serviceName]) {
-      return this.app.services[packageName][serviceName];
-    } else {
-      throw new Error(`Service ${packageName} ${serviceName} is not found.`);
-    }
+  getServiceClass(...args) {
+    return this.app.getServiceClass(...args);
   },
 
   /**
@@ -48,9 +21,7 @@ module.exports = {
   getService(packageName, serviceName) {
     assert(packageName, 'Package name cannot be empty!');
     assert(serviceName, 'Service name cannot be empty!');
-    if (this.app.services
-      && this.app.services[packageName]
-      && this.app.services[packageName][serviceName]) {
+    if (this.app.services && this.app.services[packageName] && this.app.services[packageName][serviceName]) {
       const ServiceClass = this.app.services[packageName][serviceName];
       return new ServiceClass(this);
     } else {
@@ -58,16 +29,8 @@ module.exports = {
     }
   },
 
-  getLib(packageName, libName) {
-    assert(packageName, 'Package name cannot be empty!');
-    assert(libName, 'Lib name cannot be empty!');
-    if (this.app.libs
-      && this.app.libs[packageName]
-      && this.app.libs[packageName][libName]) {
-      return this.app.libs[packageName][libName];
-    } else {
-      throw new Error(`Lib ${packageName} ${libName} is not found.`);
-    }
+  getLib(...args) {
+    return this.app.getLib(...args);
   }
 
 };
