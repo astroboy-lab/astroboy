fd1e03d1010172e1b2fa14894940abdab2a5e11e
kk <kk@youzan.com>
2019-02-21 11:32:27 +0800

    add AstroboyBootLoader

--- loader/AstroboyBootLoader.js
+++ loader/AstroboyBootLoader.js
@@ -1,9 +1,17 @@
 'use strict';
+const lodash = require('lodash');
+const assert = require('assert');
 const Loader = require('../core/Loader');
 
 class AstroboyBootLoader extends Loader {
-  load(dirs, options = {}, app) {
-    
+  load() {
+    this.globDirs(this.config.pattern, entries => {
+      entries.forEach(entry => {
+        let boot = require(entry);
+        assert(lodash.isFunction(boot), `${entry} must return a function.`);
+        boot(this.app);
+      });
+    });
   }
 }
 
