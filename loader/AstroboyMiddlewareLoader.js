fc66d1c3240b9aee2b10a690c2c14c048274771f
kk <kk@youzan.com>
2019-02-21 11:15:53 +0800

    update AstroboyMiddlewareLoader

--- loader/AstroboyMiddlewareLoader.js
+++ loader/AstroboyMiddlewareLoader.js
@@ -1,9 +1,11 @@
 'use strict';
+const path = require('path');
 const lodash = require('lodash');
 const Loader = require('../core/Loader');
 
 class AstroboyMiddlewareLoader extends Loader {
   load() {
+    // 加载中间件配置
     let config = {};
     this.globDirs(this.config.configPattern, entries => {
       entries.forEach(entry => {
@@ -11,6 +13,16 @@ class AstroboyMiddlewareLoader extends Loader {
       });
     });
     this.app.middlewareConfig = config;
+
+    // 加载中间件
+    let middlewares = {};
+    this.globDirs(this.config.pattern, entries => {
+      entries.forEach(entry => {
+        const key = this.resolveExtensions(path.basename(entry));
+        middlewares[key] = require(entry);
+      });
+    });
+    this.app.middlewares = middlewares;
   }
 }
 
