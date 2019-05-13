598894068862972c8b46d4f0a3cef45004035b67
Big Mogician <sunaoran@youzan.com>
2019-05-13 10:45:47 +0800

    fix: ctx and app extends

--- definitions/index.ts
+++ definitions/index.ts
@@ -8,8 +8,8 @@ import {
   IAstroboyPluginCtxMixins,
 } from './plugins';
 import { IBaseMiddlewareConfig, IBaseProjectConfig, IBasePluginConfig } from './config';
-import { IAstroboyCtxExtends } from './extends/context';
-import { IAstroboyAppExtends } from './extends/app';
+import { IPureAstroboyContext } from './extends/context';
+import { IPureAstroboyApplication } from './extends/app';
 
 export {
   IBaseApplication,
@@ -31,12 +31,65 @@ export {
 } from './config';
 export * from './base';
 
-export interface IAstroboyApplication<CONFIG extends any = PureObject>
+/**
+ * ## astroboy 基础app结构
+ *
+ * @author Big Mogician
+ * @export
+ * @interface IAstroboyApplication
+ * @extends {IAstroboyPluginAppMixins}
+ * @extends {IAstroboyAppExtends<CONFIG>}
+ * @template CONFIG
+ */
+export interface IAstroboyApplication<CONFIG extends PureObject = PureObject>
   extends IAstroboyPluginAppMixins,
-    IAstroboyAppExtends<CONFIG> {}
-export interface IAstroboyContext<CONFIG extends any = PureObject, A extends any = IBaseApplication<CONFIG>>
-  extends IAstroboyPluginCtxMixins,
-    IAstroboyCtxExtends<CONFIG, A> {}
+    IPureAstroboyApplication<CONFIG> {}
+
+/**
+ * astroboy 基础Ctx结构
+ *
+ * @author Big Mogician
+ * @export
+ * @interface IAstroboyContext
+ * @extends {IAstroboyPluginCtxMixins}
+ * @extends {IAstroboyCtxExtends<CONFIG, A>}
+ * @template CONFIG
+ * @template A
+ */
+export interface IAstroboyContext<
+  CONFIG extends PureObject = PureObject,
+  A extends IBaseApplication = IBaseApplication<CONFIG>
+> extends IAstroboyPluginCtxMixins, IPureAstroboyContext<CONFIG, A> {}
+
+/**
+ * astroboy 基础configs结构
+ *
+ * @author Big Mogician
+ * @export
+ * @interface IAstroboyConfigs
+ * @extends {IAstroboyPluginConfigMixins}
+ * @extends {IBaseProjectConfig}
+ */
 export interface IAstroboyConfigs extends IAstroboyPluginConfigMixins, IBaseProjectConfig {}
+
+/**
+ * astroboy 基础plugins结构
+ *
+ * @author Big Mogician
+ * @export
+ * @interface IAstroboyPlugins
+ * @extends {IAstroboyPluginRegisterMixins}
+ * @extends {IBasePluginConfig}
+ */
 export interface IAstroboyPlugins extends IAstroboyPluginRegisterMixins, IBasePluginConfig {}
+
+/**
+ * astroboy 基础middlewares结构
+ *
+ * @author Big Mogician
+ * @export
+ * @interface IAstroboyMiddlewares
+ * @extends {IAstroboyPluginMiddlewareMixins}
+ * @extends {IBaseMiddlewareConfig}
+ */
 export interface IAstroboyMiddlewares extends IAstroboyPluginMiddlewareMixins, IBaseMiddlewareConfig {}
