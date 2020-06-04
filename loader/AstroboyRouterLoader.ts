4a3d4300c0d6978a2a4f67f2a32fac4e32971f91
kk <kk@youzan.com>
2020-06-04 16:43:10 +0800

    路由支持对象描述

--- loader/AstroboyRouterLoader.ts
+++ loader/AstroboyRouterLoader.ts
@@ -1,10 +1,12 @@
 import * as fs from 'fs-extra';
 import * as glob from 'fast-glob';
+import * as lodash from 'lodash';
 // @ts-ignore typings missing
 import * as methods from 'methods';
 import { Loader } from '../core/Loader';
 import { IInnerApplication } from '../definitions/core';
 import { IOptions } from '../definitions/config';
+import { IConstructor } from '../definitions';
 
 class AstroboyRouterLoader extends Loader<Partial<IOptions>, IInnerApplication<Partial<IOptions>>> {
   load() {
@@ -24,19 +26,49 @@ class AstroboyRouterLoader extends Loader<Partial<IOptions>, IInnerApplication<P
     const controllers = this.app.controllers;
     let newRouters: any[] = [];
     routers.forEach(router => {
-      // 如果第一个参数不是 routerName，则添加空参数名
-      if (methods.indexOf(router[0].toLowerCase()) > -1) {
-        router.unshift('');
+      if (Array.isArray(router)) {
+        // 如果第一个参数不是 routerName，则添加空参数名
+        if (methods.indexOf(router[0].toLowerCase()) > -1) {
+          router.unshift('');
+        }
+        newRouters.push({
+          version: 'v1',
+          name: router[0],
+          method: router[1].toLowerCase(),
+          path: Array.isArray(router[2]) ? router[2] : [router[2]],
+          controllerClass: controllers[router[3]],
+          controllerMethod: Array.isArray(router[4]) ? router[4] : [router[4]],
+          controllerName: router[3],
+        });
+      } else {
+        newRouters.push({
+          version: 'v2',
+          name: router.name || '',
+          method: router.method.toLowerCase(),
+          path: Array.isArray(router.path) ? router.path : [router.path],
+          controllerClass: controllers[router.controllerName],
+          controllerMethod: Array.isArray(router.controllerMethod)
+            ? router.controllerMethod
+            : [router.controllerMethod],
+          controllerName: router.controllerName,
+        });
       }
-      newRouters.push({
-        version: 'v1',
-        name: router[0],
-        method: router[1].toLowerCase(),
-        path: Array.isArray(router[2]) ? router[2] : [router[2]],
-        controllerClass: controllers[router[3]],
-        controllerMethod: Array.isArray(router[4]) ? router[4] : [router[4]],
-        controllerName: router[3],
-      });
+    });
+    newRouters.forEach(item => {
+      const ControllerClass: IConstructor<any> = item.controllerClass;
+      if (!ControllerClass) {
+        throw new Error(`注册路由失败，控制器类 ${item.controllerName} 不存在`);
+      }
+      if (!lodash.isFunction(ControllerClass)) {
+        throw new Error(`注册路由失败，控制器类 ${item.controllerName} 必须为一个 class`);
+      }
+      for (let i = 0; i < item.controllerMethod.length; i++) {
+        if (!ControllerClass.prototype[item.controllerMethod[i]]) {
+          throw new Error(
+            `注册路由失败，控制器类 ${item.controllerName} 不存在方法名为 ${item.controllerMethod[i]} 的方法`
+          );
+        }
+      }
     });
     this.app.routers = newRouters;
   }
--- plugins/astroboy-router/app/middlewares/astroboy-router.ts
+++ plugins/astroboy-router/app/middlewares/astroboy-router.ts
@@ -1,7 +1,6 @@
 /**
  * 框架路由中间件
  */
-import * as lodash from 'lodash';
 // @ts-ignore typings missed
 import * as KoaRouter from 'koa-router';
 import * as compose from 'koa-compose';
@@ -14,48 +13,29 @@ const factory: MiddlewareFactory<any, IInnerApplication> = function(options = {}
 
   app.routers.forEach((router: any) => {
     const ControllerClass: IConstructor<any> = router.controllerClass;
-    if (ControllerClass) {
-      if (lodash.isFunction(ControllerClass)) {
+    router.path.forEach((item: any) => {
+      koaRouter[router.method](router.name, item, async function(ctx: any, next: () => Promise<any>) {
+        const controller = new (<any>ControllerClass)(ctx);
+        // init 是 Controller 类初始化后调用的一个方法
+        if (ControllerClass.prototype.init) {
+          router.controllerMethod.unshift('init');
+        }
         for (let i = 0; i < router.controllerMethod.length; i++) {
-          if (!ControllerClass.prototype[router.controllerMethod[i]]) {
-            throw new Error(
-              `注册路由失败，method:${router.method} path:${router.path}, method:${router.controllerMethod[i]} is not found.`
-            );
+          const method = router.controllerMethod[i];
+          const beforeMethod = 'before' + method.slice(0, 1).toUpperCase() + method.slice(1);
+          if (ControllerClass.prototype[beforeMethod]) {
+            router.controllerMethod.splice(i, 0, beforeMethod);
+            i++;
           }
         }
-        router.path.forEach((item: any) => {
-          koaRouter[router.method](router.name, item, async function(ctx: any, next: () => Promise<any>) {
-            const controller = new (<any>ControllerClass)(ctx);
-            // init 是 Controller 类初始化后调用的一个方法
-            if (ControllerClass.prototype.init) {
-              await controller['init']();
-            }
-            if (ctx.status !== 301 && ctx.status !== 302) {
-              for (let i = 0; i < router.controllerMethod.length; i++) {
-                let method = router.controllerMethod[i];
-                const beforeMethod = 'before' + method.slice(0, 1).toUpperCase() + method.slice(1);
-                if (ControllerClass.prototype[beforeMethod]) {
-                  await controller[beforeMethod]();
-                }
-                if (ctx.status !== 301 && ctx.status !== 302 && !ctx.body) {
-                  await controller[method](ctx, next);
-                } else {
-                  break;
-                }
-              }
-            }
-          });
-        });
-      } else {
-        throw new Error(
-          `注册路由失败，method:${router.method} path:${router.path}, controllerName:${router.controllerName} is not a function.`
-        );
-      }
-    } else {
-      throw new Error(
-        `注册路由失败，method:${router.method} path:${router.path}, controllerName:${router.controllerName} is undefined.`
-      );
-    }
+        for (let i = 0; i < router.controllerMethod.length; i++) {
+          await controller[router.controllerMethod[i]](ctx, next);
+          if (ctx.status === 301 || ctx.status === 302 || ctx.body) {
+            break;
+          }
+        }
+      });
+    });
   });
 
   let fn = compose([koaRouter.routes(), koaRouter.allowedMethods()]);
